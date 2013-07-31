4f07139415f637d2a5df32d7fdbf22cbb77209a3
Jens Jap <jensjap@gmail.com>
2013-07-31 15:19:59 -0400

    Makes corrections to runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=/home/jensjap/Hive/Ruby/SRDR_ImportProject/Project_2/data/*.html
+FILES=/home/jensjap/Hive/Ruby/Rails/SRDR_ImportProject/Project_2/data/*.html
 for f in $FILES
 do
     ruby main.rb --file "$f"
